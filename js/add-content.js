8bf79f27b10ae763b5a7a6d9d6b3748d2f0dfc16
sayefdeen <sayefdeen.alrawad@gmail.com>
2020-06-23 17:06:00 +0300

    adding a js file and a css file

---
+++ js/add-content.js
@@ -0,0 +1,15 @@
+"use strict";
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18) {
+  greeting = "Good evening, Class!";
+} else if (hourNow > 12) {
+  greeting = "Good afternoon, Class!";
+} else if (hourNow >= 0) {
+  greeting = "Good morning!";
+} else {
+  greeting = "Something went wrong";
+}
+// console.log(hourNow);
+document.write("<h3>" + greeting + `</h3>`);
